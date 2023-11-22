2aea38c1e14d0d3cfc24ae596a1d4dae02c98e28
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2023-11-22 21:42:35 +0800

    Add files via upload

---
+++ ChooseYourOwnDestiny/ChooseYourOwnDestiny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChooseYourOwnDestiny
+//
+//  Created by yucian huang on 2023/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
